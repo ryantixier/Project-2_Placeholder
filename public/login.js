a572be03a0d423013013cfec11e4b3a28c0ef0c7
Jordan Olguin <jordanolguin@iMac.local>
2023-06-29 12:58:08 -0600

    added login.js

---
+++ public/login.js
@@ -0,0 +1,27 @@
+const loginFormHandler = async function (event) {
+  event.preventDefault();
+
+  const usernameEl = document.querySelector("#usernameInputLogin");
+  const passwordEl = document.querySelector("#passwordInputLogin");
+
+  const response = await fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    //this will be where we send the user after logging in
+    //.replace() is making a GET request to homepage URL
+    document.location.replace("/");
+  } else {
+    alert("Failed to login");
+  }
+};
+
+document
+  .querySelector("#loginForm")
+  .addEventListener("submit", loginFormHandler);
