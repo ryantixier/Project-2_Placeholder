3229075da79d51c097cc2ddec3682a25cd279722
Jordan Olguin <jordanolguin@iMac.local>
2023-06-29 12:59:57 -0600

    added logout page

---
+++ public/logout.js
@@ -0,0 +1,15 @@
+const logout = async function () {
+  const response = await fetch("/api/user/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    //this will send user to login/sign up page
+    document.location.replace("/");
+  } else {
+    alert("Failed to log out");
+  }
+};
+
+document.querySelector("#logoutLink").addEventListener("click", logout);
