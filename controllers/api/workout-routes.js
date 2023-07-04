2c2c0b4177b25f69389b632372235d78f30ac99c
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-07-04 14:11:17 -0400

    Added route /new-workout to create new workouts, which links to a user and associates with exercises through personalBest. Added JS to handle form data and send an AJAX post request to /new-workout. Added JS to dynamically add and remove exercise inputs in the workout form.

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -27,6 +27,42 @@ router.get("/past-workouts", async (req, res) => {
     res.status(500).json(err);
   }
 });
+//route to create a new workout
+router.post("/new-workout", async (req, res) => {
+  try {
+    //select fields we want from req.body using object destructuring
+    const { comments, duration, calories_burned, exercises } = req.body;
+    //create a new workout instance
+    const newWorkout = await Workout.create({
+      //ensure workout is asscoiated with the user creating it
+      user_id: req.session.userId,
+      comments,
+      duration,
+      calories_burned,
+    });
+    //loop through each of our exercises
+    for (let exercise of exercises) {
+      const existingExercise = await Exercise.findByPk(exercise.id);
+
+      if (existingExercise) {
+        //associate the new workout and exercise with addExercise method using our personalBest through table to connect our tables
+        await newWorkout.addExercise(existingExercise, {
+          through: {
+            record_value: exercise.record_value,
+            record_unit: exercise.record_unit,
+          },
+        });
+      } else {
+        console.log(`No exercise found.`);
+      }
+    }
+
+    res.status(200).json(newWorkout);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/:id", async (req, res) => {
   try {
     const workout = await Workout.findByPk(req.params.id, {
