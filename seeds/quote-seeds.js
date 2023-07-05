79355adadf2e8c2e040e22ea3f222ecf0277ef44
corynko <ethankotel@gmail.com>
2023-07-05 11:29:49 -0600

    added new quote scenes

--- seeds/quote-seeds.js
+++ seeds/quote-seeds.js
@@ -2,40 +2,56 @@ const { Quote } = require("../models");
 
 const quoteData = [
   {
-    author: "Jackie Robinson",
-    text: "A life is not important, except in the impact it has on other lives.",
+    author: "Robin Sharma",
+    text: "If you don’t make time for exercise, you’ll probably have to make time for illness.",
   },
   {
     author: "Thomas Hobbes",
     text: "I am about to take my last voyage, a great leap in the dark.",
   },
   {
-    author: "Ralph Waldo Emerson",
-    text: "What forests of laurel we bring, and the tears of mankind, to those who stood firm against the opinion of their contemporaries!",
+    author: "Abraham Lincoln",
+    text: "Dead last finish is greater than did not finish, which trumps did not start.",
   },
   {
-    author: "E. W. Howe",
-    text: "Instead of loving your enemies - treat your friends a little better.",
+    author: "Michael John Bobak",
+    text: "Rome wasn't built in a day, but they worked on it every single day.",
+  },
+  {
+    author: "Michael John Bobak",
+    text: "All progress takes place outside the comfort zone.",
   },
   {
     author: "Paulo Coelho",
     text: "One is loved because one is loved. No reason is needed for loving.",
   },
   {
-    author: "John F. Kennedy",
-    text: "The goal of education is the advancement of knowledge and the dissemination of truth.",
+    author: "Wayne Dyer",
+    text: "Go the extra mile. It's never crowded.",
   },
   {
     author: "Oliver Goldsmith",
     text: "Hope, like the gleaming taper's light, adorns and cheers our way; and still, as darker grows the night, emits a brighter ray.",
   },
   {
-    author: "William Allen White",
-    text: "Reason has never failed men. Only force and repression have made the wrecks in the world.",
+    author: "Babe Ruth",
+    text: "You just can't beat the person who never gives up.",
   },
   {
-    author: "Douglas Adams",
-    text: "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
+    author: "Sly Stallone",
+    text: "Success is usually the culmination of controlling failure.",
+  },
+  {
+    author: "Muhammad Ali",
+    text: "I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'",
+  },
+  {
+    author: "Buddah",
+    text: "To keep the body in good health is a duty. Otherwise we shall not be able to keep our mind strong and clear.",
+  },
+  {
+    author: "Matt McGorry",
+    text: "The mind is the most important part of achieving any fitness goal. Mental change always comes before physical change.",
   },
 ];
 
