ef65f360b314b13530a311ad1f550a08b076bb52
Marlena Hooker Moore <132782689+Ms-Marlena@users.noreply.github.com>
2023-07-06 10:10:02 -0600

    used SVG file and updated animation

--- public/js/anime.js
+++ public/js/anime.js
@@ -1,9 +1,41 @@
+// anime({
+//   targets: ".animeLilGuy",
+//   translateX: "500",
+//   direction: "normal",
+//   easing: "spring(10, 80, 30, 0)",  
+// });
+
+// anime({
+//   targets: '.animeLilGuy',
+//   translateX: {
+//     value: 500,
+//     duration: 800
+//   },
+//   scale: {
+//     value: 1.5,
+//     duration: 1600,
+//     delay: 800,
+//     easing: 'easeInOutQuart'
+//   },
+//   // delay: 250 // All properties except 'scale' inherit 250ms delay
+// });
+// SWOOPY LILGUY
 anime({
   targets: ".animeLilGuy",
-  // width: "85%", // -> from '28px' to '100%',
-  // easing: "linear",
-  // direction: "alternate",
-  translateX: "500",
-  direction: "normal",
-  easing: "spring(1, 80, 10, 0)",
-});
+  translateX: 500,
+  scale: [
+    { value: .25},
+    { value: 1.2 }
+  ],
+  duration: 2000,
+  easing: "easeInOutSine",
+  direction: "reverse",
+  
+})
+
+// anime({
+//   targets: ".animeLilGuy",
+//   translateX: 250,
+//   scale: [
+//     { value: .95 }
+//   ]
